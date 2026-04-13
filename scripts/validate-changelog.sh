14d02981ec476a263c93e3ae19489ad4f461e839
Salah-Eddine Saakoun <salah-eddine.saakoun@consensys.net>
2026-04-13 23:50:10 +0200

    revert: remove --checkDeps from validate-changelog.sh
    Keep dependency bump validation opt-in via @metamaskbot check-deps only.

--- scripts/validate-changelog.sh
+++ scripts/validate-changelog.sh
@@ -10,16 +10,8 @@ fi
 package_name="$1"
 shift  # remove package name from arguments
 
-# Enable --checkDeps only on non-main branches to avoid
-# "HEAD is the same as the base branch" errors on main.
-branch=$(git rev-parse --abbrev-ref HEAD)
-check_deps_args=()
-if [[ "$branch" != "main" && "$branch" != "HEAD" ]]; then
-  check_deps_args=(--checkDeps)
-fi
-
 if [[ "${GITHUB_REF:-}" =~ '^release/' ]]; then
-  yarn auto-changelog validate --prettier --tag-prefix "${package_name}@" --rc "${check_deps_args[@]+"${check_deps_args[@]}"}" "$@"
+  yarn auto-changelog validate --prettier --tag-prefix "${package_name}@" --rc "$@"
 else
-  yarn auto-changelog validate --prettier --tag-prefix "${package_name}@" "${check_deps_args[@]+"${check_deps_args[@]}"}" "$@"
+  yarn auto-changelog validate --prettier --tag-prefix "${package_name}@" "$@"
 fi
